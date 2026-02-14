1f34617e29f2f7870aab6bee3e47427ecc2fe7a5
nithin-yz <nithinkyz@gmail.com>
2026-02-14 16:43:41 +0530

    terms added

--- app/(user)/terms-and-conditions/page.tsx
+++ app/(user)/terms-and-conditions/page.tsx
@@ -31,7 +31,7 @@ const TermsAndConditionsPage = () => {
                 <p className="mt-3">By visiting our site and/ or purchasing something from us, you engage in our &quot;Service&quot; and agree to be bound by the following terms and conditions (&quot;Terms of Service&quot;, &quot;Terms&quot;), including those additional terms and conditions and policies referenced herein and/or available by hyperlink. These Terms of Service apply to all users of the site, including without limitation users who are browsers, vendors, customers, merchants, and/ or contributors of content.</p>
                 <p className="mt-3">Please read these Terms of Service carefully before accessing or using our website. By accessing or using any part of the site, you agree to be bound by these Terms of Service. If you do not agree to all the terms and conditions of this agreement, then you may not access the website or use any services. If these Terms of Service are considered an offer, acceptance is expressly limited to these Terms of Service.</p>
                 <p className="mt-3">Any new features or tools which are added to the current store shall also be subject to the Terms of Service. You can review the most current version of the Terms of Service at any time on this page. We reserve the right to update, change or replace any part of these Terms of Service by posting updates and/or changes to our website. It is your responsibility to check this page periodically for changes. Your continued use of or access to the website following the posting of any changes constitutes acceptance of those changes.</p>
-                <p className="mt-3">Our store is hosted on Shopify Inc. They provide us with the online e-commerce platform that allows us to sell our products and services to you.</p>
+                {/* <p className="mt-3">Our store is hosted on Shopify Inc. They provide us with the online e-commerce platform that allows us to sell our products and services to you.</p> */}
               </div>
             </div>
 
@@ -187,7 +187,7 @@ const TermsAndConditionsPage = () => {
             <div className={sectionClass}>
               <h2 className={headingClass}>SECTION 18 - GOVERNING LAW</h2>
               <div className={bodyClass}>
-                <p>These Terms of Service and any separate agreements whereby we provide you Services shall be governed by and construed in accordance with the laws of Germany.</p>
+                <p>These Terms of Service and any separate agreements whereby we provide you Services shall be governed by and construed in accordance with the laws of Singapore.</p>
               </div>
             </div>
 
